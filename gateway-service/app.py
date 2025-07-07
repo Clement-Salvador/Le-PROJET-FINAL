aabdf5a79145c6c28a4d8f273e97ed7cdb93ae12
Restoueix Emilien <restouee@3il.fr>
2025-07-07 09:34:27 +0200

    add home route

--- gateway-service/app.py
+++ gateway-service/app.py
@@ -2,9 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Bienvenue sur la Gateway Flask !"
+
 @app.route('/health', methods=['GET'])
 def get_health():
     try:
         return jsonify({'status': 'OK'}), 200
     except Exception as e:
         return jsonify({'status': 'KO', 'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5004)
